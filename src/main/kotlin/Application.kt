87a76f7431e4a8509e2ba606ce8ed768846075e8
yaBabyIlyha <ilya.tatarinov77@gmail.com>
2025-05-21 23:39:04 +0300

    небольшой фикс для отчета

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -12,30 +12,17 @@ import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    // Сначала подключаемся к БД
     Database.connect(
         url = "jdbc:postgresql://localhost:5432/mydb",
         driver = "org.postgresql.Driver",
         user = "postgres",
         password = "9626"
     ).also {
-        // Добавляем логгер
         transaction {
             addLogger(StdOutSqlLogger)
         }
         println("Database connected successfully")
     }
-
-    transaction {
-        // Простой тестовый запрос
-        val result = exec("SELECT 1") { rs ->
-            rs.next()
-            rs.getInt(1)
-        }
-        println("Database test query result: $result")
-    }
-
-    // Затем запускаем сервер
     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
